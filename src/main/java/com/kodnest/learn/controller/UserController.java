4d2b64451bb0a9a239304298e6270fdedf4ac363
🎯✨Peetla Mahesh💫🤖 <21cs042@acetcbe.edu.in>
2026-03-09 19:16:32 +0530

    Update UserController.java

--- src/main/java/com/kodnest/learn/controller/UserController.java
+++ src/main/java/com/kodnest/learn/controller/UserController.java
@@ -10,8 +10,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.demo.entity.User;
-import com.example.demo.service.UserService;
+import com.kodnest.learn.entity.User;
+import com.kodnest.learn.service.UserService;
 
 @RestController
 @RequestMapping("/api/users")
