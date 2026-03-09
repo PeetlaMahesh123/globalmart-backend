1fb3040845d8d1d9eb1183faaa30a3f8c5edbbff
🎯✨Peetla Mahesh💫🤖 <21cs042@acetcbe.edu.in>
2026-03-09 18:41:36 +0530

    Update OrderItem.java

--- src/main/java/com/kodnest/learn/entity/OrderItem.java
+++ src/main/java/com/kodnest/learn/entity/OrderItem.java
@@ -1,5 +1,6 @@
 package com.kodnest.learn.entity;
 
+
 import jakarta.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -34,63 +35,51 @@ public class OrderItem {
 	}
     // Getters and setters
 
-	public OrderItem(int id, Order order, int productId, int quantity, BigDecimal pricePerUnit, BigDecimal totalPrice) {
-		super();
-		this.id = id;
-		this.order = order;
-		this.productId = productId;
-		this.quantity = quantity;
-		this.pricePerUnit = pricePerUnit;
-		this.totalPrice = totalPrice;
-	}
+    public int getId() {
+        return id;
+    }
 
-	public int getId() {
-		return id;
-	}
+    public void setId(int id) {
+        this.id = id;
+    }
 
-	public void setId(int id) {
-		this.id = id;
-	}
+    public Order getOrder() {
+        return order;
+    }
 
-	public Order getOrder() {
-		return order;
-	}
+    public void setOrder(Order order) {
+        this.order = order;
+    }
 
-	public void setOrder(Order order) {
-		this.order = order;
-	}
+    public int getProductId() {
+        return productId;
+    }
 
-	public int getProductId() {
-		return productId;
-	}
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
 
-	public void setProductId(int productId) {
-		this.productId = productId;
-	}
+    public int getQuantity() {
+        return quantity;
+    }
 
-	public int getQuantity() {
-		return quantity;
-	}
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 
-	public void setQuantity(int quantity) {
-		this.quantity = quantity;
-	}
+    public BigDecimal getPricePerUnit() {
+        return pricePerUnit;
+    }
 
-	public BigDecimal getPricePerUnit() {
-		return pricePerUnit;
-	}
+    public void setPricePerUnit(BigDecimal pricePerUnit) {
+        this.pricePerUnit = pricePerUnit;
+    }
 
-	public void setPricePerUnit(BigDecimal pricePerUnit) {
-		this.pricePerUnit = pricePerUnit;
-	}
+    public BigDecimal getTotalPrice() {
+        return totalPrice;
+    }
 
-	public BigDecimal getTotalPrice() {
-		return totalPrice;
-	}
-
-	public void setTotalPrice(BigDecimal totalPrice) {
-		this.totalPrice = totalPrice;
-	}
-    
-
+    public void setTotalPrice(BigDecimal totalPrice) {
+        this.totalPrice = totalPrice;
+    }
 }
