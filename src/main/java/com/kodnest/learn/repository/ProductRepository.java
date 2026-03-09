c7e394bb33277d135ec1c27808cb50b542601573
🎯✨Peetla Mahesh💫🤖 <21cs042@acetcbe.edu.in>
2026-03-09 18:50:49 +0530

    Update ProductRepository.java

--- src/main/java/com/kodnest/learn/repository/ProductRepository.java
+++ src/main/java/com/kodnest/learn/repository/ProductRepository.java
@@ -1,6 +1,5 @@
 package com.kodnest.learn.repository;
 
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
