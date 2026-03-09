80b0981bfbb4de3d5b99ebf5d31c4130560b75b9
🎯✨Peetla Mahesh💫🤖 <21cs042@acetcbe.edu.in>
2026-03-09 19:22:51 +0530

    Update CartItemRepository.java

--- src/main/java/com/kodnest/learn/repository/CartItemRepository.java
+++ src/main/java/com/kodnest/learn/repository/CartItemRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.repository.query.Param;
 
-import com.example.demo.entity.CartItem;
+import com.kodnest.learn.entity.CartItem;
 
 @Repository
 public interface CartItemRepository extends JpaRepository<CartItem, Integer> {
