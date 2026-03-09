91c7e535d0cf4ee6aa17d529beac298c8d72cdd4
🎯✨Peetla Mahesh💫🤖 <21cs042@acetcbe.edu.in>
2026-03-09 14:41:36 +0530

    Update UserRepository.java

--- src/main/java/com/kodnest/learn/repository/UserRepository.java
+++ src/main/java/com/kodnest/learn/repository/UserRepository.java
@@ -3,12 +3,15 @@ package com.kodnest.learn.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.kodnest.learn.entity.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByUsername(String username);
 
     Optional<User> findByEmail(String email);
 
-    Optional<User> findByUsername(String username);
 }
