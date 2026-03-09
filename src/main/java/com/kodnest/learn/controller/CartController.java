6fd61cf0340b15eb9d96b21c0e67c40680db99e8
🎯✨Peetla Mahesh💫🤖 <21cs042@acetcbe.edu.in>
2026-03-09 19:04:33 +0530

    Update CartController.java

--- src/main/java/com/kodnest/learn/controller/CartController.java
+++ src/main/java/com/kodnest/learn/controller/CartController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:5174", allowCredentials = "true")
 @RequestMapping("/api/cart")
 public class CartController {
 
