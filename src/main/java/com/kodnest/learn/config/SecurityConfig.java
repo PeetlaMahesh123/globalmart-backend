2855fab30322b342ab11ad09d2ecbdaa10feb3db
🎯✨Peetla Mahesh💫🤖 <21cs042@acetcbe.edu.in>
2026-03-10 21:12:24 +0530

    Update SecurityConfig.java

--- src/main/java/com/kodnest/learn/config/SecurityConfig.java
+++ src/main/java/com/kodnest/learn/config/SecurityConfig.java
@@ -30,7 +30,6 @@ public class SecurityConfig {
         return http.build();
     }
 
-    // ✅ CORS configuration
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
 
@@ -42,10 +41,11 @@ public class SecurityConfig {
         ));
 
         configuration.setAllowedMethods(Arrays.asList(
-                "GET", "POST", "PUT", "DELETE", "OPTIONS"
+                "GET","POST","PUT","DELETE","OPTIONS"
         ));
 
         configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setExposedHeaders(Arrays.asList("*"));
 
         configuration.setAllowCredentials(true);
 
