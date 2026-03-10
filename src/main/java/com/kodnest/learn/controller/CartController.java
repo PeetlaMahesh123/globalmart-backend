d46966908e33923e786afa4a6441a5f29658a3e8
🎯✨Peetla Mahesh💫🤖 <21cs042@acetcbe.edu.in>
2026-03-10 15:08:02 +0530

    Update CartController.java

--- src/main/java/com/kodnest/learn/controller/CartController.java
+++ src/main/java/com/kodnest/learn/controller/CartController.java
@@ -2,8 +2,10 @@ package com.kodnest.learn.controller;
 
 import com.kodnest.learn.entity.User;
 import com.kodnest.learn.service.CartService;
-import com.kodnest.learn.repository.UserRepository;
 
+import jakarta.servlet.http.HttpServletRequest;
+
+import com.kodnest.learn.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
