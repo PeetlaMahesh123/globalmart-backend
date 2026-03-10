a57ff954d655d835842de03340c04597407372d7
🎯✨Peetla Mahesh💫🤖 <21cs042@acetcbe.edu.in>
2026-03-10 15:31:13 +0530

    Update AuthController.java

--- src/main/java/com/kodnest/learn/controller/AuthController.java
+++ src/main/java/com/kodnest/learn/controller/AuthController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import com.kodnest.learn.dto.LoginRequest;
+import org.springframework.http.HttpStatus;
 import com.kodnest.learn.entity.User;
 import com.kodnest.learn.service.AuthService;
 
